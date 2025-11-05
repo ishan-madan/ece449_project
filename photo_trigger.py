eb2b6a723aba32fa9f58d6e247c2bd7dc4f3dcdf
TheGoonSesh <TheGoonSesh@gmail.com>
2025-11-05 15:37:46 +0000

    wifi triggering in photo trigger and new ml model weights uploaded@

--- photo_trigger.py
+++ photo_trigger.py
@@ -1,11 +1,14 @@
 import RPi.GPIO as GPIO
 import subprocess
 import time
+import requests
 from datetime import datetime
 from lightweightml import detect_animal
 
+"21: Front PIR(1),26: PIR(2),20: 3, 16: Back PIR(4)"
+
 GPIO_PINS = [20]
-CAM_ASSIGN = {20:'0', 21:'1', 26:'2'}
+CAM_ASSIGN = {20:'0', 21:'1', 26:'2', 16:'0'}
 DEBOUNCE_TIME = 0.2
 PHOTO_DIR = "test_photos/"
 
@@ -34,12 +37,18 @@ def scan_gpio_pins():
 
 
 try:
-	print(f"MOnitoring GPIO pins {GPIO_PINS}...")
+	print(f"Monitoring GPIO pins {GPIO_PINS}...")
 	while True:
 		active_pin = scan_gpio_pins()
 		if (active_pin != -1):
 			print(f"HIGH DETECTED FROM PIN {active_pin}")
-			print(take_photo(active_pin))
+			detectionResults = take_photo(active_pin)
+			print(detectionResults)
+			if detectionResults["animal_detected"] == True:
+				print(f"DETER!!! RUN!!!")
+				resp = requests.get("http://192.168.68.106/on", timeout = 5)
+				print(f"sent deter signal")
+
 			while GPIO.input(active_pin) == GPIO.HIGH:
 				time.sleep(DEBOUNCE_TIME)
 			print("ALL PINS SCANNING AGAIN")
